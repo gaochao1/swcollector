1b9f9c543436fc395896e5a91a67a6e13e655c8a
gaochao1 <gaochao1@gmail.com>
2015-10-27 18:34:16 +0800

    Revert "Add ConnectionStat，displayByBit"

--- g/const.go
+++ g/const.go
@@ -7,8 +7,7 @@ import (
 // changelog:
 // 3.1.3: code refactor
 // 3.1.4: bugfix ignore configuration
-// 3.1.5: more sw support, DisplayByBit cfg
 const (
-	VERSION          = "3.1.5"
+	VERSION          = "3.1.4"
 	COLLECT_INTERVAL = time.Second
 )
