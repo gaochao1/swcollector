0ad3f3bfa8e2989d60ff70ea845c55f0b6c51f0c
Feng_Qi <freedomkk_qfeng@qq.com>
2015-11-02 14:33:36 +0800

    fix ping timeout bug

--- funcs/swping.go
+++ funcs/swping.go
@@ -38,12 +38,13 @@ func pingMetrics(ip string, ch chan SwPing) {
 	rtt, err := sw.PingRtt(ip, timeout)
 	if err != nil {
 		log.Println(ip, err)
-		swPing.Ping = 0
+		swPing.Ip = -1
+		swPing.Ping = rtt
+		ch <- swPing
+	}else{
+		swPing.Ip = ip
+		swPing.Ping = rtt
+		ch <- swPing
 	}
-
-	swPing.Ip = ip
-	swPing.Ping = rtt
-	ch <- swPing
-
 	return
 }
