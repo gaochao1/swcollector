eb6da52ffa3a3c2ce787c880796033f0c8c44e18
dotwoo <dotwoo@gmail.com>
2016-11-17 10:47:03 +0800

    修复本地交换机扫描错误地将linux服务器判断为交换机

--- g/const.go
+++ g/const.go
@@ -15,8 +15,9 @@ import (
 // 4.3.8: 删除外调snmpwalk方式。
 // 4.5.0: 重构代码使结构清晰一点
 // 4.5.22: 添加raven 异常信息收集，pfc统计
+// 4.5.23: 修复本地交换机扫描错误将linux服务器判断为交换机
 
 const (
-	VERSION          = "4.5.22"
+	VERSION          = "4.5.23"
 	COLLECT_INTERVAL = time.Second
 )
